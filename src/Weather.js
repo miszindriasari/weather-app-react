760ed5f6870dbd0d28089866e2419061af2eee41
miszindriasari <82860575+miszindriasari@users.noreply.github.com>
2022-04-08 19:43:34 +0200

    Added axios imigration shows weather of the city

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp} C`
+    );
+  }
+  let apiKey = "92adabc89647e51a6b2ac793bd82529e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <h1>Hello</h1>;
+}
